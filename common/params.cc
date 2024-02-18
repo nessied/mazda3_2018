150aa4fc4b19664c7b9feef9390050f2d5f43bfe
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-18 15:11:30 -0700

    Use EV gas lookup tables
    Co-Authored-By: Tim Wilson <7284371+twilsonco@users.noreply.github.com>
    Co-Authored-By: Eric Brown <13560103+nworb-cire@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -265,6 +265,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"DriveStats", PERSISTENT},
     {"DynamicPathWidth", PERSISTENT},
     {"EngageVolume", PERSISTENT},
+    {"EVTable", PERSISTENT},
     {"ExperimentalModeActivation", PERSISTENT},
     {"ExperimentalModeViaLKAS", PERSISTENT},
     {"ExperimentalModeViaScreen", PERSISTENT},
