7e84754c6b892b32fea94f3352c0ef6a923361a2
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-18 15:11:30 -0700

    Torque lateral jerk with stock torque controller
    Add toggle to use Twilsonco's  lateral jerk with stock torque controller.
    
    Co-Authored-By: Tim Wilson <7284371+twilsonco@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -386,6 +386,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Updated", PERSISTENT},
     {"UpdateSchedule", PERSISTENT},
     {"UpdateTime", PERSISTENT},
+    {"UseLateralJerk", PERSISTENT},
     {"UseSI", PERSISTENT},
     {"UseVienna", PERSISTENT},
     {"WarningSoftVolume", PERSISTENT},
