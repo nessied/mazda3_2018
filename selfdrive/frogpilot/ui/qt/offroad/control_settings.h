2e0d1ed3897b07a19f55e22b2445297b88adac74
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-09 19:49:07 -0700

    FrogPilot setup - Toggle panels

---
+++ selfdrive/frogpilot/ui/qt/offroad/control_settings.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <set>
+
+#include "selfdrive/ui/qt/offroad/settings.h"
+#include "selfdrive/ui/ui.h"
+
+class FrogPilotControlsPanel : public FrogPilotListWidget {
+  Q_OBJECT
+
+public:
+  explicit FrogPilotControlsPanel(SettingsWindow *parent);
+
+signals:
+  void openParentToggle();
+  void openSubParentToggle();
+
+private:
+  void hideSubToggles();
+  void hideToggles();
+  void showEvent(QShowEvent *event, const UIState &s);
+  void updateCarToggles();
+  void updateMetric();
+  void updateState(const UIState &s);
+
+  ButtonControl *deleteModelBtn;
+  ButtonControl *downloadModelBtn;
+  ButtonControl *selectModelBtn;
+  ButtonControl *slcPriorityButton;
+
+  FrogPilotDualParamControl *aggressiveProfile;
+  FrogPilotDualParamControl *conditionalSpeedsImperial;
+  FrogPilotDualParamControl *conditionalSpeedsMetric;
+  FrogPilotDualParamControl *relaxedProfile;
+  FrogPilotDualParamControl *standardProfile;
+  FrogPilotDualParamControl *trafficProfile;
+
+  FrogPilotParamManageControl *modelManagerToggle;
+
+  FrogPilotParamValueToggleControl *steerRatioToggle;
+
+  std::set<QString> aolKeys = {"AlwaysOnLateralMain", "HideAOLStatusBar", "PauseAOLOnBrake"};
+  std::set<QString> conditionalExperimentalKeys = {"CECurves", "CECurvesLead", "CENavigation", "CESignal", "CESlowerLead", "CEStopLights", "HideCEMStatusBar"};
+  std::set<QString> deviceManagementKeys = {"DeviceShutdown", "IncreaseThermalLimits", "LowVoltageShutdown", "NoLogging", "NoUploads", "OfflineMode"};
+  std::set<QString> experimentalModeActivationKeys = {"ExperimentalModeViaDistance", "ExperimentalModeViaLKAS", "ExperimentalModeViaTap"};
+  std::set<QString> laneChangeKeys = {"LaneChangeTime", "LaneDetectionWidth", "OneLaneChange"};
+  std::set<QString> lateralTuneKeys = {"ForceAutoTune", "NNFF", "NNFFLite", "SteerRatio", "TacoTune", "TurnDesires"};
+  std::set<QString> longitudinalTuneKeys = {"AccelerationProfile", "AggressiveAcceleration", "DecelerationProfile", "LeadDetectionThreshold", "SmoothBraking", "StoppingDistance", "TrafficMode"};
+  std::set<QString> mtscKeys = {"DisableMTSCSmoothing", "MTSCAggressiveness", "MTSCCurvatureCheck"};
+  std::set<QString> qolKeys = {"CustomCruise", "CustomCruiseLong", "MapGears", "OnroadDistanceButton", "PauseLateralSpeed", "ReverseCruise", "SetSpeedOffset"};
+  std::set<QString> speedLimitControllerKeys = {"SLCControls", "SLCQOL", "SLCVisuals"};
+  std::set<QString> speedLimitControllerControlsKeys = {"Offset1", "Offset2", "Offset3", "Offset4", "SLCFallback", "SLCOverride", "SLCPriority"};
+  std::set<QString> speedLimitControllerQOLKeys = {"ForceMPHDashboard", "SetSpeedLimit", "SLCConfirmation", "SLCLookaheadHigher", "SLCLookaheadLower"};
+  std::set<QString> speedLimitControllerVisualsKeys = {"ShowSLCOffset", "SpeedLimitChangedAlert", "UseVienna"};
+  std::set<QString> tuningKeys = {"kiV1", "kiV2", "kiV3", "kiV4", "kpV1", "kpV2", "kpV3", "kpV4"};
+  std::set<QString> visionTurnControlKeys = {"CurveSensitivity", "DisableVTSCSmoothing", "TurnAggressiveness"};
+
+  std::map<std::string, ParamControl*> toggles;
+
+  Params params;
+  Params paramsMemory{"/dev/shm/params"};
+
+  bool hasAutoTune;
+  bool hasCommaNNFFSupport;
+  bool hasNNFFLog;
+  bool hasOpenpilotLongitudinal;
+  bool hasPCMCruise;
+  bool hasDashSpeedLimits;
+  bool isMetric = params.getBool("IsMetric");
+  bool isRelease;
+  bool isToyota;
+  bool started;
+
+  float steerRatioStock;
+};
