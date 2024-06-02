3a18321d3076d5598dede44a01ee041cf0d3a3d8
garrettpall <76917194+garrettpall@users.noreply.github.com>
2024-06-02 19:13:45 -0400

    Remove time LKA time check

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -69,7 +69,7 @@ class CarController(CarControllerBase):
     # Avoid GM EPS faults when transmitting messages too close together: skip this transmit if we
     # received the ASCMLKASteeringCmd loopback confirmation too recently
     last_lka_steer_msg_ms = (now_nanos - CS.loopback_lka_steering_cmd_ts_nanos) * 1e-6
-    if (self.frame - self.last_steer_frame) >= steer_step and last_lka_steer_msg_ms > MIN_STEER_MSG_INTERVAL_MS:
+    if (self.frame - self.last_steer_frame) >= steer_step and (last_lka_steer_msg_ms > MIN_STEER_MSG_INTERVAL_MS or sc_car):
       # Initialize ASCMLKASteeringCmd counter using the camera until we get a msg on the bus
       if CS.loopback_lka_steering_cmd_ts_nanos == 0:
         self.lka_steering_cmd_counter = CS.pt_lka_steering_cmd_counter + 1
