9a6db1734d29e727cbee44e0d7a6135b72552e98
garrettpall <76917194+garrettpall@users.noreply.github.com>
2024-06-16 16:45:17 -0400

    Add 2019 Volt

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -247,6 +247,11 @@ class CarInterface(CarInterfaceBase):
     elif candidate == CAR.CADILLAC_CT6_CC:
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
+    elif candidate == CAR.CHEVROLET_VOLT_2019:
+      ret.steerActuatorDelay = 0.2
+      ret.minSteerSpeed = 10 * CV.KPH_TO_MS
+      CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+
     if ret.enableGasInterceptor:
       ret.networkLocation = NetworkLocation.fwdCamera
       ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM
