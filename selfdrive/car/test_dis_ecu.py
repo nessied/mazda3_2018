c97d3c29cb7e627e85c32549b144961838b6759d
garrettpall <76917194+garrettpall@users.noreply.github.com>
2024-07-02 19:32:05 -0400

    Revert "Update test_dis_ecu.py"
    This reverts commit ec6c87c1052388690fab1dc66857c9dc77cce650.

--- selfdrive/car/test_dis_ecu.py
+++ selfdrive/car/test_dis_ecu.py
@@ -13,12 +13,17 @@ if __name__ == "__main__":
   ADDR_OFFSET = 0x400
   EXT_DIAG_REQUEST = b"\x10\x03"
   COM_CONT_REQUEST = b"\x28"
+  TESTER_REQUEST = b"\x01\x3E"
+
 
   panda = Panda()
   panda.set_safety_mode(Panda.SAFETY_ELM327)
 
-#   isotp_send(panda, EXT_DIAG_REQUEST, ECU_ADDR, bus=BUS)
-#   hexdump(isotp_recv(panda, ECU_ADDR + ADDR_OFFSET, bus=BUS))
+  isotp_send(panda, EXT_DIAG_REQUEST, ECU_ADDR, bus=BUS)
+  hexdump(isotp_recv(panda, ECU_ADDR + ADDR_OFFSET, bus=BUS))
+
+  isotp_send(panda, TESTER_REQUEST, ECU_ADDR, bus=BUS)
+  hexdump(isotp_recv(panda, ECU_ADDR + ADDR_OFFSET, bus=BUS))
 
   isotp_send(panda, COM_CONT_REQUEST, ECU_ADDR, bus=BUS)
   hexdump(isotp_recv(panda, ECU_ADDR + ADDR_OFFSET, bus=BUS))
